97da9b9df2390c7f04990590ed14825fca0c91c5
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2017-10-31 00:25:19 +0530

    First milestone

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: ES6', pages: '110' },
+        { title: 'Harry Potter', pages: '88' },
+        { title: 'React: Learn by Fun', pages: '130' },
+        { title: 'Eloquent Ruby', pages: '50' },
+    ];
+}
